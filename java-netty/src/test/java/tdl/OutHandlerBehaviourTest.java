58677a00e1eefb3346caf4a1549ea0b6872c8c7e
Carl-Erik Kopseng <carlerik@gmail.com>
2018-06-05 14:05:52 +0200

    Added a promise listener test for ctx.write

--- java-netty/src/test/java/tdl/OutHandlerBehaviourTest.java
+++ java-netty/src/test/java/tdl/OutHandlerBehaviourTest.java
@@ -11,7 +11,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class OutHandlerBehaviourTest {
 
     static ChannelHandlerAdapter dummyOutHandler() {
-        return new ChannelHandlerAdapter() {
+        return new ChannelDuplexHandler() {
+            @Override
+            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                super.channelRead(ctx, msg);
+            }
+
+            @Override
+            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                super.write(ctx, msg, promise);
+            }
         };
     }
 
@@ -79,5 +88,36 @@ public class OutHandlerBehaviourTest {
         // channel.checkException(); // for inbound exceptions
         String result = channel.readOutbound();
         assertThat(result).isNull();
+    }
+
+    @Test
+    public void netty_promises_can_be_used_to_listen_for_next_handlers_success() {
+        final ChannelOutboundHandlerAdapter handlerWithListener = new ChannelOutboundHandlerAdapter() {
+            @Override
+            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
+                ctx.write(msg, promise.addListener((ChannelFutureListener) future -> {
+                    if (!future.isSuccess()) {
+                        // can't get past the handler throwing the exception further up (nearer the head)
+                        // so we need to remove it to be able to return a result
+                        ctx.pipeline().removeFirst();
+
+                        ctx.writeAndFlush("removed offender");
+                    } else {
+                        System.err.println("Should never get here");
+                    }
+                }));
+            }
+        };
+
+        final EmbeddedChannel channel = new EmbeddedChannel(
+                errorThrowingOutHandler(),
+                handlerWithListener,
+                mirrorInHandler()
+        );
+
+        channel.writeInbound("hello");
+        // channel.checkException(); // for inbound exceptions
+        String result = channel.readOutbound();
+        assertThat(result).isEqualTo("removed offender");
     }
 }
