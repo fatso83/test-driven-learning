e7d8d422bbae387add0436e400e202d9dc8bd5fd
Carl-Erik Kopseng <carlerik@gmail.com>
2024-09-23 14:08:04 +0200

    Add @EnableAsync

--- java-spring/src/test/kotlin/io/github/fatso83/spring_learning/SpringAsyncTest.kt
+++ java-spring/src/test/kotlin/io/github/fatso83/spring_learning/SpringAsyncTest.kt
@@ -2,14 +2,15 @@ package io.github.fatso83.spring_learning
 
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.Async
+import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import org.springframework.stereotype.Component
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ExecutionException
-
 
 @SpringJUnitConfig(SpringAsyncTest.MyTestConfig::class)
 class SpringAsyncTest {
@@ -20,8 +21,8 @@ class SpringAsyncTest {
     @Autowired
     lateinit var scheduler: ManualScheduler
 
-
-    @TestConfiguration()
+    @Configuration
+    @EnableAsync
     class MyTestConfig {
         @Bean(name = ["myThreadPoolExecutor"])
         fun asyncExecutor(): ThreadPoolTaskExecutor {
